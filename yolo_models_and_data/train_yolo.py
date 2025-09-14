aec71e458e3639b46102528f6f89aaba1e48574d
rutvim2009 <rutvim2009@gmail.com>
2025-09-14 23:09:24 +0000

    made train_yolo.py

---
+++ yolo_models_and_data/train_yolo.py
@@ -0,0 +1,20 @@
+from ultralytics import YOLO
+import os
+
+def train_yolo():
+    data_yaml = os.path.join("synthetic_odlc", "adlc_data.yaml")
+
+    model = YOLO("yolov8n.pt")
+
+    model.train(
+        data=data_yaml,
+        epochs=10,
+        imgsz=640,
+        batch=16,
+        project="runs_odlc",
+        name="exp",
+        exist_ok=True
+    )
+
+if __name__ == "__main__":
+    train_yolo()
