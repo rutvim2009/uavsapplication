dfc4f755e039d69dee2b61099f36e68d692729c3
rutvim2009 <rutvim2009@gmail.com>
2025-09-15 01:15:20 +0000

    made inference code, but issue in imwrite

---
+++ yolo_models_and_data/inference_yolo.py
@@ -0,0 +1,19 @@
+from ultralytics import YOLO
+import cv2
+import os
+
+def run_inference(model_path="/workspaces/uavsapplication/yolo_models_and_data/runs_odlc/exp/weights/best.pt", source="/workspaces/uavsapplication/yolo_models_and_data/yolo_model_test_images"):
+    model = YOLO(model_path)
+    images = [source]
+
+    os.makedirs("predictions", exist_ok=True)
+
+    for image in images:
+        results = model(image)
+        annotated = results[0].plot() 
+        out_path = os.path.join("predictions", os.path.basename(image))
+        cv2.imwrite(out_path, annotated)
+        print(f"saved: {out_path}")
+
+if __name__ == "__main__":
+    run_inference()
