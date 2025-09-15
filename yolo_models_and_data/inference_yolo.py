694afe4590060c22764e87256ca1b8c140244189
rutvim2009 <rutvim2009@gmail.com>
2025-09-15 04:56:26 +0000

    made filename.out for each prediction

--- yolo_models_and_data/inference_yolo.py
+++ yolo_models_and_data/inference_yolo.py
@@ -15,5 +15,17 @@ def run_inference(model_path="/workspaces/uavsapplication/yolo_models_and_data/r
         cv2.imwrite(out_path, annotated)
         print(f"saved: {out_path}")
 
+        filename = os.path.splitext(os.path.basename(image))[0]
+        out_txt_path = os.path.join("predictions", filename + ".out")
+        with open(out_txt_path, "w") as f:
+            if len(results[0].boxes) == 0:
+                f.write("Blank\n")
+            else:
+                best_box = results[0].boxes[results[0].boxes.conf.argmax()]
+                x1, y1, x2, y2 = best_box.xyxy.cpu().numpy().flatten()
+                xc = int((x1 + x2) / 2)
+                yc = int((y1 + y2) / 2)
+                f.write(f"{xc} {yc}\n")
+
 if __name__ == "__main__":
     run_inference()
